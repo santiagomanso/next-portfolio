004430d40c1142b703e5587d92f0d2485b203188
santiagomanso <santiagomanso@web.de>
2023-11-20 17:03:48 +0100

    ✨ update: typewriter / css update

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const AppContainer = ({ children }: Props) => {
   return (
-    <main className='h-screen bg-gradient-to-br dark:bg-gradient-to-bl transition-colors ease-in-out duration-500 from-white via-white to-gray-900 dark:from-neutral-900  dark:to-zinc-800 flex justify-center items-center overflow-y-auto overflow-x-hidden'>
+    <main className='h-screen bg-gradient-to-br dark:bg-gradient-to-bl transition-colors ease-in-out duration-500 from-gray-200 via-white to-gray-900 dark:from-neutral-900  dark:to-zinc-800 flex justify-center items-center overflow-y-auto overflow-x-hidden'>
       <section className='flex flex-col h-full max-w-[85rem] lg:justify-center w-full lg:py-5 relative'>
         {children}
       </section>
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,8 +1,8 @@
 interface Props {
   children: React.ReactNode
   border?: boolean
-  gap?: string
-  justifyCenter?: string
+  gap?: boolean
+  justifyCenter?: boolean
   padding?: string
 }
 
@@ -15,12 +15,12 @@ const Container = ({
 }: Props) => {
   return (
     <section
-      className={` bg-gradient-to-tl
-       from-gray-200 via-gray-300 to-gray-500  lg:rounded-sm  transition-colors ease-in-out duration-500  dark:from-zinc-900/70 dark:to-neutral-700 flex flex-col   h-full lg:h-3/4 lg:p-0 -z-1 overflow-auto items-center
+      className={`animate-fade animate-duration-400 animate-ease-out bg-gradient-to-tl
+       to-white via-gray-100 from-gray-400 lg:rounded-sm  transition-colors ease-in-out duration-500  dark:from-zinc-900/70 dark:to-neutral-700 flex flex-col h-full lg:h-3/4 lg:p-0 -z-1 overflow-auto items-center
        ${padding ? padding : 'px-2 py-5 lg:px-5 lg:py-4'}
       ${
         border
-          ? 'lg:border lg:border-neutral-900 lg:dark:border-neutral-700'
+          ? 'lg:border shadow-lg dark:shadow-neutral-900 lg:border-gray-500 lg:dark:border-neutral-700'
           : ''
       }
       ${gap ? gap : 'gap-10'}
