4dcd74068e99e372d208f1ac165442bc9b64ad90
Santiago Manso Castro <santiagomanso.dev@gmail.com>
2024-01-29 21:38:49 +0100

    refactor useTheme function

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useTheme } from 'next-themes'
 import React, { SetStateAction, useEffect, useState } from 'react'
 
 interface Props {
@@ -10,32 +11,15 @@ interface Props {
 }
 
 const ThemeSwitcher = ({ setOpen, isResponsive, isClicked }: Props) => {
-  const [isSelected, setIsSelected] = useState('dark')
-  const handleSelect = () => {
-    if (setOpen) {
-      //setOpen(false)
-    }
-    isSelected === 'dark' ? setIsSelected('light') : setIsSelected('dark')
+  const { theme, setTheme } = useTheme()
+
+  const handleClick = () => {
+    return theme === 'dark' ? setTheme('light') : setTheme('dark')
   }
-
-  useEffect(() => {
-    isSelected === 'dark'
-      ? (document.documentElement.classList.add('dark'), setIsSelected('dark'))
-      : (document.documentElement.classList.remove('dark'),
-        setIsSelected('light'))
-  }, [isSelected])
-
-  useEffect(() => {
-    if (isClicked) {
-      setIsSelected('dark')
-    } else {
-      setIsSelected('light')
-    }
-  }, [isClicked])
 
   return (
     <div
-      onClick={handleSelect}
+      onClick={handleClick}
       className={`flex transition-colors rounded-full ${
         isResponsive
           ? 'bg-gradient-to-br from-slate-800 to-blue-800 dark:from-green-500 dark:to-teal-900 border h-[25px] px-2 border-sky-100 dark:border-green-300 w-[64px]'
@@ -44,10 +28,10 @@ const ThemeSwitcher = ({ setOpen, isResponsive, isClicked }: Props) => {
     >
       <div
         className={`transition-all ease-in-out duration-300 flex justify-center items-center ${
-          isSelected === 'dark' ? 'translate-x-[32px] ' : 'translate-x-0 '
+          theme === 'dark' ? 'translate-x-[32px] ' : 'translate-x-0 '
         } `}
       >
-        {isSelected === 'dark' ? (
+        {theme === 'dark' ? (
           <FontAwesomeIcon
             icon={faSun}
             className={`text-lg cursor-pointer  ${
