1a81130345dc721d01114b00b9a03ecb43b1a6e8
santiagomanso <santiagomanso.dev@gmail.com>
2024-10-30 02:18:06 +0100

    Feat: SEO OG meta tag image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,25 @@ import Container from '@/components/container';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
 import LaptopHome from './_components/laptop';
 import ResponsiveHome from './_components/responsive';
+import Head from 'next/head';
 
 export default function Home() {
   const isDesktop = useMediaQuery('(min-width: 768px)');
 
   return (
-    <Container
-      border
-      background
-      justifyCenter
-      padding='p-2 lg:px-5 lg:py-4'
-      withImage //NOTE image on bottom right only on desktop
-    >
-      {isDesktop ? <LaptopHome /> : <ResponsiveHome />}
-    </Container>
+    <>
+      <Head>
+        <meta property='og:image' content='/og/og.png' />
+      </Head>
+      <Container
+        border
+        background
+        justifyCenter
+        padding='p-2 lg:px-5 lg:py-4'
+        withImage //NOTE image on bottom right only on desktop
+      >
+        {isDesktop ? <LaptopHome /> : <ResponsiveHome />}
+      </Container>
+    </>
   );
 }
